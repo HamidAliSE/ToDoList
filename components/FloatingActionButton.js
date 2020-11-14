17ba81c43a435bc68bbc818fbddb53d430fd3c21
Hamid Ali <hamidalig942@gmail.com>
2020-11-14 17:55:46 +0500

    change FAB icon source.

--- components/FloatingActionButton.js
+++ components/FloatingActionButton.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { FAB } from "react-native-paper";
 import { StyleSheet } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
 
 import { AddTask } from "../resources/constants/strings";
 
 const FloatingActionButton = (props) => {
   return (
     <FAB
+      icon="add"
+      label={AddTask}
       onPress={props.onPress}
       style={styles.floatingActionButton}
-      icon={() => <Icon name="add" size={24} />}
-      label={AddTask}
     ></FAB>
   );
 };
