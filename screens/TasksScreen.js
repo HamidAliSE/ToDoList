564ad0cf9a83bef05c2fd6053bbe8cafad03b2b5
Hamid Ali <hamidalig942@gmail.com>
2020-11-16 10:17:51 +0500

    list footer component style updated.

--- screens/TasksScreen.js
+++ screens/TasksScreen.js
@@ -251,7 +251,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
   },
   footer: {
-    marginVertical: 36,
+    marginVertical: 32,
   },
   appBarActionContainer: {
     flexDirection: "row",
